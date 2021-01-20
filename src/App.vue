7bd5103dc315a298f735fe9eb7eab0628fa9ce8b
Caruychen <caruychen@gmail.com>
2021-01-20 14:47:23 +0200

    navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link> -->
-    </div>
-    <router-view/>
+    <TheNavbar />
+    <router-view />
   </div>
 </template>
 
+<script>
+import TheNavbar from '@/components/TheNavbar'
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
 <style>
 @import "assets/css/style.css";
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
 </style>
