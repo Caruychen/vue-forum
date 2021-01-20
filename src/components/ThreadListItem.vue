70758b28e0165a63b827b3b327c13693db211574
Caruychen <caruychen@gmail.com>
2021-01-20 15:05:09 +0200

    add utility

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -25,7 +25,7 @@
 </template>
 
 <script>
-
+import { countObjectProperties } from '@/utils'
 export default {
   props: {
     thread: {
@@ -35,7 +35,7 @@ export default {
   },
   computed: {
     repliesCount () {
-      return Object.keys(this.thread.posts).length - 1
+      return countObjectProperties(this.thread.posts) - 1
     },
     user () {
       return this.$store.state.users[this.thread.userId]
