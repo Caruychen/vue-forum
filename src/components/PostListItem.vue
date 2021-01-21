da197a3e752764015be66d6b675ce29a14a18264
Caruychen <caruychen@gmail.com>
2021-01-21 14:29:01 +0200

    post editing

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -3,29 +3,44 @@
     <div class="user-info">
       <a href="#" class="user-name">{{ user.name }}</a>
       <a href="#">
-        <img
-          :src="user.avatar"
-          alt=""
-          class="avatar-large"
-        />
+        <img :src="user.avatar" alt="" class="avatar-large" />
       </a>
       <p class="desktop-only text-small">{{ userPostsCount }} posts</p>
     </div>
 
     <div class="post-content">
-      <div>
-        {{ post.text }}
+      <template v-if="!editing">
+        <div>
+          {{ post.text }}
+        </div>
+        <a
+          @click.prevent="editing = true"
+          href="#"
+          style="margin-left: auto"
+          class="link-unstyled"
+          title="Make a change"
+          ><i class="fa fa-pencil"></i
+        ></a>
+      </template>
+      <div v-else>
+        <PostEditor
+          :post="post"
+          @save="editing = false"
+          @cancel="editing = false"
+        />
       </div>
     </div>
 
-    <div class="post-date text-faded" >
-      <AppDate :timeStamp="post.publishedAt"/>
+    <div class="post-date text-faded">
+      <div v-if="post.edited" class="edition-info">edited</div>
+      <AppDate :timeStamp="post.publishedAt" />
     </div>
   </div>
 </template>
 
 <script>
 import { countObjectProperties } from '@/utils'
+import PostEditor from './PostEditor'
 export default {
   props: {
     post: {
@@ -33,6 +48,16 @@ export default {
       type: Object
     }
   },
+  components: {
+    PostEditor
+  },
+
+  data () {
+    return {
+      editing: false
+    }
+  },
+
   computed: {
     user () {
       return this.$store.state.users[this.post.userId]
